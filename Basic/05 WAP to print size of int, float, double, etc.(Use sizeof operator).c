10da901664194f0d83c0bc08cfc46745dc9f96a5
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-01-20 11:36:17 +0530

    Basic program

---
+++ Basic/05 WAP to print size of int, float, double, etc.(Use sizeof operator).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\n==================================");
+    printf("\n Size of int => %d", sizeof(int));
+    printf("\n Size of float => %d", sizeof(float));
+    printf("\n Size of double => %d",sizeof(double));
+    printf("\n Size of char => %d", sizeof(char));
+    printf("\n==================================");
+    return 0;
+}
