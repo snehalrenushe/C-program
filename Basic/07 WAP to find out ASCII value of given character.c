7afed1ba241034e2aa9d1f84da405389ee7e7495
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-01-21 09:59:35 +0530

    Basic program

---
+++ Basic/07 WAP to find out ASCII value of given character.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    char c;
+    printf("\n ASCII value of given character =>");
+    scanf("%c",&c);
+    if((c>='A'&&c<='Z')||(c>='a'&&c<='z'))
+    {
+       printf("\n %d=%c ", c,c );
+    }
+     else
+     {
+         printf("\n invalid input!!!!!");
+     }
+     return 0;
+}
