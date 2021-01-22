d0eb12adef1e7166e6181b425670bf50163632ea
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-01-22 19:31:57 +0530

    Conditional Statement & Loops

---
+++ Conditional Statement & Loops/01-2 WAP to find maximum from 2 numbers(using if..else).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int No1,No2;
+    up:
+    printf("\n==========================");
+    printf("\n Enter two No => ");
+    scanf("%d%d",&No1,&No2);
+    printf("==========================");
+    printf("\n No1 = %d   No2 = %d \n",No1,No2);
+    if(No1==No2)
+    {
+        printf("\n Numbers are Equal...!");
+        goto up;
+    }
+     printf("\n==========================");
+    if(No1>No2)
+    {
+        printf("\n No1 is Maximum...");
+    }
+    else
+    {
+        printf("\n No2 is Maximum...");
+    }
+     printf("\n==========================");
+    return 0;
+}
