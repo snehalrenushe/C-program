5504219ae9990c5a107efc2ee6b4ec69918dec9d
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-01-22 19:22:40 +0530

    Conditional Statement & Loops

---
+++ Conditional Statement & Loops/01 WAP to find maximum from 2 numbers (Use ternary operator).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int No1,No2;
+    printf("\n Enter two number => ");
+    scanf("%d%d",&No1,&No2);
+    printf("========================\n");
+    printf(" N1= %d    N2= %d \n",No1,No2);
+    No1<No2?printf("\n N2 is Greater..."): printf("\n N1 is Greater...");
+    printf("\n========================");
+    return 0;
+}
