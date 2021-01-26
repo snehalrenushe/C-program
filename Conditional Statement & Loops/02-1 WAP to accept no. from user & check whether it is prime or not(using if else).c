823596c7cc36a06b058306334b377d7d6b7b6092
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-01-26 14:30:17 +0530

    Conditional Statement & Loops

---
+++ Conditional Statement & Loops/02-1 WAP to accept no. from user & check whether it is prime or not(using if else).c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+  int No1,y,cnt=0;
+  up:
+  printf("\n enter two no to check its prime or not => ");
+  scanf("%d",&No1);
+   if(No1==1||No1==0)
+  {
+    printf("\n its a natural Number... ");
+    goto up;
+  }
+  for(y=2;y<=No1/2;y++)
+  {
+     if(No1%y==0)
+     {
+        cnt=1;
+     }
+  }
+
+  if(cnt==0)
+  {
+      printf("\n its a prime Number... ");
+  }
+  else
+  {
+      printf("\n its not a prime Number...");
+  }
+  return 0;
+}
