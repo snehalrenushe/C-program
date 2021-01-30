317f21f52b756907760920629669f0a81230f7ed
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-01-30 23:20:19 +0530

    Conditional Statement & Loops

---
+++ Conditional Statement & Loops/11  WAP to display digit from 4 digit inputted no. order right to left.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    int No=0,temp=0,rev=0,Dcnt=0;
+    printf("\n================================================================================\n");
+    printf("\n Enter number => ");
+    scanf("%d",&No);
+    printf("\n================================================================================\n");
+    for(temp=No;temp!=0;temp=temp/10)
+    {
+       Dcnt++;
+    }
+    if(Dcnt==4)
+    {
+        for(temp=No;temp!=0;temp=temp/10)
+        {
+            rev=(rev*10)+(temp%10);
+        }
+        printf("\n  The Inputted Number is = %d \n\n And \n\n  its Reverse Number is = %d",No,rev);
+    }
+    else
+    {
+        printf("\n Invalid Number...!");
+    }
+    printf("\n================================================================================");
+
+    return 0;
+}
