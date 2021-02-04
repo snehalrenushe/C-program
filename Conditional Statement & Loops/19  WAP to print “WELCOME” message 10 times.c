c73de3c1deb51fc14b9dc373dd94e59fe8ddd4ec
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-02-04 16:25:28 +0530

    Conditional Statement & Loops

---
+++ Conditional Statement & Loops/19  WAP to print “WELCOME” message 10 times.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+ int main()
+ {
+     int i=0;
+     printf("\n==================================");
+     while(i<10)
+     {
+         printf("\n Wel-Come...!");
+         i++;
+     }
+     printf("\n==================================");
+     return 0;
+ }
