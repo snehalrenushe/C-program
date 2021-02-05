9d8d508dd47875ccde09f9acca9a6a2a51e131be
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-02-05 12:38:34 +0530

    Conditional Statement & Loops

---
+++ Conditional Statement & Loops/20  WAP to print characters A to Y.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    char Ch='A';
+    printf("\n============================================");
+    printf("\n A to Y Characters are as follows => ");
+    for(Ch='A';Ch<='Y';Ch++)
+    {
+        printf("\n\t\t\t\t     %c ",Ch);
+
+    }
+    printf("\n============================================\n");
+    return 0;
+}
