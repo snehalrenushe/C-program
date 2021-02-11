6a39ab40c49c0ddd2f1e7fece068b4c15c687bbc
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-02-11 23:02:32 +0530

    Conditional Statement & Loops

---
+++ Conditional Statement & Loops/28  WAP to find minimum & maximum no. from 10 inputted numbers.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int No=0,i=1,Max=0,Min=0;
+    printf("\==============================================");
+    for(i=1;i<=10;i++)
+    {
+        printf("\n Enter %d Number => ",i);
+        scanf("%d",&No);
+        if(i==1)
+        {
+            Max=No;
+            Min=No;
+            continue;
+        }
+       if(No>Max)
+        {
+            Max=No;
+        }
+      else if(No<Min)
+        {
+            Min=No;
+        }
+    }
+    printf("\===================================================");
+    printf("\n The Maximum Number is %d \n The Minimum Number is %d \n",Max,Min);
+    printf("\===================================================");
+    return 0;
+}
