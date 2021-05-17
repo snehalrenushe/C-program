7945cd83abd6ab225e6f48691dd1d6e73a62c344
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-05-17 22:02:42 +0530

    Pattern Printing

---
+++ Pattern Printing/07  H Pattern.c
@@ -0,0 +1,67 @@
+/*
+
+                =======Given Pattern========
+
+                        *           *
+                        *           *
+                        *           *
+                        * * * * * * *
+                        *           *
+                        *           *
+                        *           *
+
+                ============================
+
+
+*/
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    int iRow=0 , iCol=0 , i=0 , j=0 ;
+
+    printf("\n Enter Number Of Rows  = ");
+    scanf("%d",&iRow);
+
+    printf("\n Enter Number Of Columns = ");
+    scanf("%d",&iCol);
+
+    printf("\n ==========Output Pattern==========\n\n");
+
+    for(i=1;i<=iRow;i++)
+    {
+
+        for(j=1;j<=iCol;j++)
+        {
+
+            if(j==1||j==iCol||i==iCol/2+1)
+            {
+
+                printf(" * ");
+
+            }
+
+            else
+            {
+
+                printf("   ");
+
+            }
+
+        }
+
+        printf("\n");
+
+    }
+
+    printf("\n =================================\n");
+
+
+    getch();
+    return 0;
+
+}
